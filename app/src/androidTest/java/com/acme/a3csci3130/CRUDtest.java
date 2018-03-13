65c01d1dc07286209fd6d5c7fa9b3f6b5f6d7855
Karthick Parameswaran <kr688977@dal.ca>
2018-03-13 14:24:26 -0300

    firebase rules implementation
    Java doc comments implementation and firebase rules implemented

--- app/src/androidTest/java/com/acme/a3csci3130/CRUDtest.java
+++ app/src/androidTest/java/com/acme/a3csci3130/CRUDtest.java
@@ -1,35 +1,30 @@
 package com.acme.a3csci3130;
-import android.os.SystemClock;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.widget.ListView;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.anything;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.Matchers.hasToString;
-import static org.hamcrest.core.StringStartsWith.startsWith;
 
 /**
  * Created by Karthick P on 3/12/2018.
+ * CRUDtest.java tests the basic CRUD operation for this application
  */
 @RunWith(AndroidJUnit4.class)
 
-public class Update_erase_detailstest {
+public class CRUDtest {
     @Rule public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
     String name1 = "test1";
     String name2 = "testupdate";
@@ -38,10 +33,16 @@ public class Update_erase_detailstest {
     String address = "No:5, South Park Street, Halifax";
     String business = "Fisher";
     String province = "NS";
+    /** Test() functions implements the test case in order 1. createtest 2. updatetest 3.delete test
+     * {@link CRUDtest#createtest()}-> creates the new contact with name as test 1
+     * {@link CRUDtest#readtest()} -> read the test 1 contact and verifies with the entered data
+     * {@link CRUDtest#updatetest()} ()} -> updates the "test1" contact name as "testupdate" and verifies it*
+     * {@link CRUDtest#deletetest()}()} -> deletes the "testupdate" contact and verifies whether it is deleted.*/
 
     @Test public void test(){
         // testcase pre-condition: delete all contacts in firebase database.
         createtest();
+        readtest();
         updatetest();
         deletetest();
     }
@@ -58,6 +59,7 @@ public class Update_erase_detailstest {
         onView(withId(R.id.submitButton)).perform(click());// -> clicks on Create Contact button in CreateContactActivity
         onView(withId(R.id.listView)).check(matches(isDisplayed()));
         onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).check(matches(withText(name1)));
+
     }
     public void readtest(){
         onView(withId(R.id.listView)).check(matches(isDisplayed()));
@@ -68,6 +70,8 @@ public class Update_erase_detailstest {
         onView(withId(R.id.address)).check(matches(withText(address)));
         onView(withId(R.id.business)).check(matches(withText(business)));
         onView(withId(R.id.province)).check(matches(withText(province)));
+        pressBack();
+
     }
 
     public void updatetest() {
